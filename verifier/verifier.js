0a381b9fa64c3c47470269a66527f9381aa407dc
Alun Evans <alune@Aluns-14-Mac.home>
2023-01-26 07:54:47 +0100

    tidy

--- verifier/verifier.js
+++ verifier/verifier.js
@@ -1,6 +1,4 @@
 /* eslint-disable no-console */
-const identity = require('freeverse-crypto-js');
-const Utils = require('web3-utils');
 const Web3 = require('web3')
 const config = require('../config.json');
 
@@ -23,6 +21,8 @@ if (ownerID_A == ownerID_B)
 const sign_message = (assetID) => {
 
   const sig = web3.eth.accounts.sign(assetID, config.user_privateKey);
+  console.log(assetID);
+  console.log(sig.signature);
   return sig.signature;
 }
 
@@ -31,40 +31,6 @@ const recover_message = (assetID, sig) => {
   return recovered;
 }
 
-
-
-const test_round_trip = async () => {
-  // Client wants to send data to User, encrypted so that only User can decrypt.
-  // User publishes his publicKey. Client encrypts using it.
-  const UserPublicKey = identity.publicKeyFromPrivateKey(user_privateKey);
-  console.log(UserPublicKey);
-  console.log(user_address);
-
-  // First client encrypts the message, the hash of which is stored on the server somewhere
-  // This is json object that Client wants to send:
-  const jsonObject = { msg: hashed_message };
-  console.log('Object that Client wants to send to User: ', jsonObject);
-
-  //Client now encrypts this
-  const encryptedString = await identity.encryptWithPublicKey(
-    JSON.stringify(jsonObject),
-    UserPublicKey,
-  );
-  console.log('Client actually sends this string: ', encryptedString);
-
-  //User decrypts the message, only they can
-  const UserPrivateKey = user_privateKey;
-
-  const decryptedString = await identity.decryptWithPrivateKey(encryptedString, UserPrivateKey);
-  const decryptedJson = JSON.parse(decryptedString);
-  console.log('User recovers the original json: ', decryptedJson);
-  if (decryptedJson.msg === hashed_message) {
-    console.log("SUCCESS: " +decryptedJson.msg);
-  }
-  else {
-    console.log("message was not as expected!");
-  }
-};
 
 exports.sign_message = sign_message;
 exports.recover_message = recover_message;
